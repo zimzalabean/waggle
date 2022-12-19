11c98f4c33ce63bdef2e4a968ac54f428b224ca8
Anna Boland <ab6@wellesley.edu>
2022-12-18 23:33:33 -0500

    changed icons, fixed my own dumb typo

--- app.py
+++ app.py
@@ -1106,7 +1106,7 @@ def dashboard():
 def init_db():
     dbi.cache_cnf()
     # set this local variable to 'wmdb' or your personal or team db
-    db_to_use = 'mp2_db' 
+    db_to_use = 'ab6_db' 
     dbi.use(db_to_use)
     print('will connect to {}'.format(db_to_use))
 
